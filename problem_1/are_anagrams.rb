3179b3c0fbc4876db407acbb97952adfa484d660
Hare Ram Rai <guru.java.ram@gmail.com>
2016-09-10 19:47:03 +0530

    Fix code indentation

--- problem_1/are_anagrams.rb
+++ problem_1/are_anagrams.rb
@@ -3,10 +3,10 @@ using StringPrime
 
 module AreAnagrams
   def self.are_anagrams? (string_a, string_b)
-		if string_a.length != string_b.length 
+    if string_a.length != string_b.length 
       false
     else
       string_a.prime_product ==  string_b.prime_product
     end
-	end
+  end
 end
--- problem_2/collection_test.rb
+++ problem_2/collection_test.rb
@@ -17,17 +17,16 @@ class CollectionTest < Test::Unit::TestCase
     result = Collection.make_flat [1, 2, 3, [4, 6, [7, 8]], 5, 9, 10]
     assert_equal [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], result
   end
-  
+
   def test_make_flat_should_flatten_the_nested_array_with_corrct_sorting
     result = Collection.make_flat [9,8, 3, [4, 10, [1, 2]], 5, 7, 6]
     assert_equal [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], result
   end
   
-
   def test_incorrect_argument_should_be_raise 
     assert_raise ArgumentTypeError do
       Collection.make_flat "string"
     end
   end
-  
+
 end
